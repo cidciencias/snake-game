afcec1827a5bced20141a1aaa3065a906cedee3e
Luis Cid <alexcid2002@gmail.com>
2024-06-02 10:46:34 +0100

    snake-game V1.1
    Implemented snake body logic. If the snake head moves, the rest of the body follows it

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import time
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments_list = []
@@ -18,8 +20,16 @@ for position in starting_positions:
 
 game_is_on = True
 while game_is_on:
-    for seg in segments_list:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments_list) - 1, 0, -1):
+        new_x = segments_list[seg_num - 1].xcor()
+        new_y = segments_list[seg_num - 1].ycor()
+        segments_list[seg_num].goto(new_x, new_y)
+    segments_list[0].forward(20)
+    segments_list[0].left(90)
+
 
 
 
