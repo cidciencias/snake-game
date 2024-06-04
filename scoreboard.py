a0e5281bd6c026712482bf985179b2a9e1b64b0a
Luis Cid <alexcid2002@gmail.com>
2024-06-04 19:10:48 +0100

    snake-game V2.3
    Implemented collisions with walls. If the skanes touches a wall its game over

--- scoreboard.py
+++ scoreboard.py
@@ -12,7 +12,7 @@ class Scoreboard(Turtle):
         self.color("white")
         self.hideturtle()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center", font=FONT)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
@@ -20,5 +20,8 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=FONT)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
